37a38362ffbf048f660dc1551963e0c86b8d1d39
miketoth <mft214@lehigh.edu>
2013-08-13 20:22:41 -0700

    Updated weather.sh to use a more robust JSON parser, but introduced a dependency to jq

--- weather.sh
+++ weather.sh
@@ -9,25 +9,8 @@ CITY=$1;
 if [ CITY ]
 then
     # curl to find current weather data and put it into JSON var
-    JSON=$(curl "http://api.openweathermap.org/data/2.5/find?q=$CITY&mode=json");
+    JSON=$(curl "http://api.openweathermap.org/data/2.5/find?q=$CITY&mode=json" | jq '.list[0]["weather"][0]["description"]' );
 fi
 
-# Set string delimiter to comma and split the result
-IFS=": ," read -ra ADDR <<< "$JSON"
-IS_WEATHER="no"
-
-
-# for each string in the array
-for i in "${ADDR[@]}"; do
-    # if IS_WEATHER has been set: output it
-    if [ "$IS_WEATHER" = "yes" ];
-        then
-            echo $i
-    fi
-
-    # check to see if we have found weather yet
-    if [ "$i" = '"weather"' ];
-        then
-            IS_WEATHER="yes"
-    fi
-done
+echo $JSON
+echo "Done"
